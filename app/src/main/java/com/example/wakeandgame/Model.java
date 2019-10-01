a25dc5027a414e7ccc6cf20e0dea73ce204c457d
Seth Heath <S400162@nwmissouri.edu>
2019-10-01 18:56:32 -0500

    Created Model class with alarm class embedded
    Created the model in which we will create the alarms with.

---
+++ app/src/main/java/com/example/wakeandgame/Model.java
@@ -0,0 +1,25 @@
+package com.example.wakeandgame;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Model {
+
+    public static class Alarm{
+        public Time time;
+        public Date date;
+        public String note;
+
+        public Alarm(){
+            this.time = null;
+            this.date = null;
+            this.note = null;
+        }
+
+        public Alarm(Time time, Date date, String note){
+            this.date = date;
+            this.time = time;
+            this.note = note;
+        }
+    }
+}

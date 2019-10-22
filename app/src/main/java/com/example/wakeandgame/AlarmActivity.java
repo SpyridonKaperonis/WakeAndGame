d854690e6368c00d01b899f0f06312725da80f57
eliross84 <eliross84@gmail.com>
2019-10-22 18:39:03 -0500

    Added intent from AlarmActivity.java to JavelinActivity.java
    Signed-off-by: eliross84 <eliross84@gmail.com>

--- app/src/main/java/com/example/wakeandgame/AlarmActivity.java
+++ app/src/main/java/com/example/wakeandgame/AlarmActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmActivity extends AppCompatActivity {
 
@@ -27,7 +29,14 @@ public class AlarmActivity extends AppCompatActivity {
 
 
         //Eli
-
+        final Intent javelinIntent = new Intent(this, JavelinActivity.class);
+        Button javelinBTN = findViewById(R.id.javelinBTN);
+        javelinBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(javelinIntent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/wakeandgame/JavelinActivity.java
+++ app/src/main/java/com/example/wakeandgame/JavelinActivity.java
@@ -2,6 +2,7 @@ package com.example.wakeandgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class JavelinActivity extends AppCompatActivity {
@@ -9,5 +10,9 @@ public class JavelinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.javelin_layout);
+
+        Intent intent = getIntent();
     }
 }

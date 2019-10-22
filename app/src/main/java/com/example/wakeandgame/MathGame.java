54bf9a609fb935a82217dbcc09b9ab1bffa72543
SpyridonKaperonis <42813038+SpyridonKaperonis@users.noreply.github.com>
2019-10-22 18:09:44 -0500

    I created the MathGame activity for the Mathgame

---
+++ app/src/main/java/com/example/wakeandgame/MathGame.java
@@ -0,0 +1,31 @@
+package com.example.wakeandgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MathGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mathgame_layout);
+
+        Button mathBTNHome = findViewById(R.id.mathGameNav);
+        mathBTNHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                startActivity(intent);
+
+            }
+
+        });
+
+
+}
+}

4a4b17c556973b3bd6444e54c79b74cb0608fc9e
eliross84 <eliross84@gmail.com>
2019-10-22 18:04:33 -0500

    Created temporary button to switch to view out alarm activity
    Signed-off-by: eliross84 <eliross84@gmail.com>

---
+++ app/src/main/java/com/example/wakeandgame/AlarmActivity.java
@@ -0,0 +1,17 @@
+package com.example.wakeandgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.alarm_layout);
+        Intent intent = getIntent();
+    }
+}

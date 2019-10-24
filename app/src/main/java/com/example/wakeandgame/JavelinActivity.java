30dc372b651bf20e215e831369a4b452390ec113
eliross84 <eliross84@gmail.com>
2019-10-23 19:37:41 -0500

    Attempting to create some kind of sliding event in order to fling the image and bring it back to its original placement. Hoping to store the location of where the image is released in order to calculate the angle of trajectory.
    Signed-off-by: eliross84 <eliross84@gmail.com>

--- app/src/main/java/com/example/wakeandgame/JavelinActivity.java
+++ app/src/main/java/com/example/wakeandgame/JavelinActivity.java
@@ -1,11 +1,24 @@
 package com.example.wakeandgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class JavelinActivity extends AppCompatActivity {
+
+    private int locationX;
+    private int locationY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,48 @@ public class JavelinActivity extends AppCompatActivity {
         setContentView(R.layout.javelin_layout);
 
         Intent intent = getIntent();
+        final ImageView characterIV = findViewById(R.id.characterIV);
+
+        //Attempting to create an event where I slide the image in order to calculate
+        //an angle for the javelin throw itself
+
+        characterIV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                final int x = (int)motionEvent.getRawX();
+                final int y = (int)motionEvent.getRawY();
+
+                switch(motionEvent.getAction()){
+
+                    case MotionEvent.ACTION_DOWN:
+                        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams)view.getLayoutParams();
+
+                        locationX = x - layoutParams.leftMargin;
+                        locationY = y - layoutParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        ConstraintLayout.LayoutParams layoutP = (ConstraintLayout.LayoutParams)view.getLayoutParams();
+
+                        layoutP.leftMargin = x - locationX;
+                        layoutP.bottomMargin = locationY - y;
+                        layoutP.rightMargin = 0;
+                        layoutP.topMargin = 0;
+
+                        characterIV.setLayoutParams(layoutP);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+
+                        break;
+
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }

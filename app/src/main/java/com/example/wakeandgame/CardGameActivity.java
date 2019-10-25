8fdf7709eaaac8d3d25cc4697cbb3d4d493fd0e1
Seth Heath <S400162@nwmissouri.edu>
2019-10-25 15:58:30 -0500

    added cardgame activity
    I created an empty activity for my card game

---
+++ app/src/main/java/com/example/wakeandgame/CardGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.wakeandgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

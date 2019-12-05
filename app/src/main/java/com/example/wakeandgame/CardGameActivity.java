60f82e6bba77f699f55348a29074573a6e2f8d6a
Seth Heath <S400162@nwmissouri.edu>
2019-12-05 16:57:21 -0600

    updated my intent
    Adjusting my intent

--- app/src/main/java/com/example/wakeandgame/CardGameActivity.java
+++ app/src/main/java/com/example/wakeandgame/CardGameActivity.java
@@ -2,6 +2,7 @@ package com.example.wakeandgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,6 +25,8 @@ public class CardGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
 
         GridView gridView = (GridView) findViewById(R.id.cardGV);
         CardAdapter cardAdapter = new CardAdapter(this);

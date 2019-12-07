7a1f388a0b834f8ce63da64c4382c1535ff85409
Ben Brown <46798091+BenB049@users.noreply.github.com>
2019-12-07 13:42:31 -0600

    added more questions to library and updated trivia code

--- app/src/main/java/com/example/wakeandgame/PicturePuzzle.java
+++ app/src/main/java/com/example/wakeandgame/PicturePuzzle.java
@@ -1,11 +1,15 @@
 package com.example.wakeandgame;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.lang.reflect.Array;
@@ -14,94 +18,181 @@ import java.util.ArrayList;
 
 public class PicturePuzzle extends AppCompatActivity {
 
+    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+
+    private TextView mScoreTV;
+    private TextView mQuestion;
+    private Button mChoice1BTN;
+    private Button mChoice2BTN;
+    private Button mChoice3BTN;
+    private Button mChoice4BTN;
+    private Button mCat1;
+    private Button mCat2;
+    private Button mCat3;
+    private Button mCat4;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNum = 0;
+    private int category = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.picture_puzzle);
 
-        //Intent intent = getIntent();
+        mScoreTV = (TextView)findViewById(R.id.scoreTV);
+        mQuestion = (TextView)findViewById(R.id.question);
+        mChoice1BTN = (Button)findViewById(R.id.answer1);
+        mChoice2BTN = (Button)findViewById(R.id.answer2);
+        mChoice3BTN = (Button)findViewById(R.id.answer3);
+        mChoice4BTN = (Button)findViewById(R.id.answer4);
+        mCat1 = (Button)findViewById(R.id.category1);
+        mCat2 = (Button)findViewById(R.id.category2);
+        mCat3 = (Button)findViewById(R.id.category3);
+        mCat4 = (Button)findViewById(R.id.category4);
 
-        final ArrayList<String> mathCategory = new ArrayList<>();
-        ArrayList<String> videoGameCategory = new ArrayList<>();
-        ArrayList<String> codingCategory = new ArrayList<>();
-        ArrayList<String> moviesCategory = new ArrayList<>();
-
-        mathCategory.add("What is the next prime number after 7?");
-        mathCategory.add("What is the square root of 144?");
-        mathCategory.add("What is the result of 3 squared?");
-        mathCategory.add("What is a polygon with 7 sides called?");
-        mathCategory.add("What does the roman numeral 'C' represent?");
-
-        videoGameCategory.add("What year was the Playstation 1 released?");
-        videoGameCategory.add("What is the name of the final course in all of the 'Mario Cart' games?");
-        videoGameCategory.add("");
-
-
-        Button category1 = findViewById(R.id.category1);
-        Button category2 = findViewById(R.id.category2);
-        Button category3 = findViewById(R.id.category3);
-        Button category4 = findViewById(R.id.category4);
-
-        final Button answer1 = findViewById(R.id.answer1);
-        final Button answer2 = findViewById(R.id.answer2);
-        final Button answer3 = findViewById(R.id.answer3);
-        final Button answer4 = findViewById(R.id.answer4);
-
-        final TextView question = findViewById(R.id.question);
-        TextView questionTV = findViewById(R.id.questionTV);
-        final TextView scoreTV = findViewById(R.id.scoreTV);
-
-        final boolean[] cat1 = {false};
-        final boolean[] cat2 = {false};
-        final boolean[] cat3 = {false};
-        final boolean[] cat4 = {false};
-
-        category1.setOnClickListener(new View.OnClickListener() {
+        mCat1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cat1[0] = true;
-                question.setText(mathCategory.get(0));
+                category = 1;
+                updateQuestionMath();
+            }
+        });
 
-                if(cat1[0] == true) {
-                    answer1.setText("one test");
-                    answer2.setText("two test");
-                    answer3.setText("three test");
-                    answer4.setText("four test");
-                }
+        mCat2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                category = 2;
+                updateQuestionVG();
+            }
+        });
 
-                answer1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        scoreTV.setText("SCORE: 1/10");
+        mChoice1BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mChoice1BTN.getText() == mAnswer) {
+                    mScore += 1;
+                    updateScore(mScore);
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
                     }
-                });
+
+
+                    Toast.makeText(PicturePuzzle.this, "Correct!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(PicturePuzzle.this, "Incorrect...", Toast.LENGTH_SHORT).show();
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+                }
             }
         });
 
-
-
-        category2.setOnClickListener(new View.OnClickListener() {
+        mChoice2BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cat2[0] = true;
+                if(mChoice2BTN.getText() == mAnswer) {
+                    mScore += 1;
+                    updateScore(mScore);
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+
+                    Toast.makeText(PicturePuzzle.this, "Correct!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(PicturePuzzle.this, "Incorrect...", Toast.LENGTH_SHORT).show();
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+                }
             }
         });
 
-        category3.setOnClickListener(new View.OnClickListener() {
+        mChoice3BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cat3[0] = true;
+                if(mChoice3BTN.getText() == mAnswer) {
+                    mScore += 1;
+                    updateScore(mScore);
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+
+                    Toast.makeText(PicturePuzzle.this, "Correct!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(PicturePuzzle.this, "Incorrect...", Toast.LENGTH_SHORT).show();
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+                }
             }
         });
 
-        category4.setOnClickListener(new View.OnClickListener() {
+        mChoice4BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cat4[0] = true;
+                if(mChoice4BTN.getText() == mAnswer) {
+                    mScore += 1;
+                    updateScore(mScore);
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+
+                    Toast.makeText(PicturePuzzle.this, "Correct!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(PicturePuzzle.this, "Incorrect...", Toast.LENGTH_SHORT).show();
+                    if(category == 1) {
+                        updateQuestionMath();
+                    } else if(category == 2) {
+                        updateQuestionVG();
+                    }
+                }
             }
         });
-
 
     }
+
+    private void updateQuestionMath() {
+        mQuestion.setText(mQuestionLibrary.getQuestionMath(mQuestionNum));
+        mChoice1BTN.setText(mQuestionLibrary.getChoice1Math(mQuestionNum));
+        mChoice2BTN.setText(mQuestionLibrary.getChoice2Math(mQuestionNum));
+        mChoice3BTN.setText(mQuestionLibrary.getChoice3Math(mQuestionNum));
+        mChoice4BTN.setText(mQuestionLibrary.getChoice4Math(mQuestionNum));
+
+        mAnswer = mQuestionLibrary.getCorrectMath(mQuestionNum);
+        mQuestionNum++;
+    }
+
+    private void updateQuestionVG() {
+        mQuestion.setText(mQuestionLibrary.getQuestionVG(mQuestionNum));
+        mChoice1BTN.setText(mQuestionLibrary.getChoice1VG(mQuestionNum));
+        mChoice2BTN.setText(mQuestionLibrary.getChoice2VG(mQuestionNum));
+        mChoice3BTN.setText(mQuestionLibrary.getChoice3VG(mQuestionNum));
+        mChoice4BTN.setText(mQuestionLibrary.getChoice4VG(mQuestionNum));
+
+        mAnswer = mQuestionLibrary.getCorrectVG(mQuestionNum);
+        mQuestionNum++;
+    }
+
+    private void updateScore(int point) {
+        mScoreTV.setText("SCORE: " + mScore + "/10");
+    }
+
+
 }
--- app/src/main/java/com/example/wakeandgame/QuestionLibrary.java
+++ app/src/main/java/com/example/wakeandgame/QuestionLibrary.java
@@ -2,7 +2,7 @@ package com.example.wakeandgame;
 
 public class QuestionLibrary {
 
-    private String mQuestions [] = {
+    private String mQuestionsMath [] = {
             "What is the next prime number after 7?",
             "What is the square root of 144?",
             "What is the result of 3 squared?",
@@ -10,7 +10,15 @@ public class QuestionLibrary {
             "What does the roman numeral 'C' represent?"
     };
 
-    private String mChoices [][] = {
+    private String mQuestionsVG [] = {
+            "What year was the Playstation 1 released in North America?",
+            "What is the name of the final course in all of the 'Mario Cart' games?",
+            "What video game studio makes the 'God of War' games?",
+            "What video game included the first 'Easter Egg'?",
+            "The first Pokemon video game appeared on what console?"
+    };
+
+    private String mChoicesMath [][] = {
             {"8", "9", "10", "11"},
             {"16", "14", "12", "10"},
             {"8", "6", "9", "5"},
@@ -18,37 +26,79 @@ public class QuestionLibrary {
             {"10", "50", "100", "200"}
     };
 
-    private String mCorrect [] = {
+    private String mChoicesVG [][] = {
+            {"1994", "1995", "1996", "1997"},
+            {"Toad's Turnpike", "Peach Beach", "Rainbow Road", "Moonview Highway"},
+            {"Sucker Punch", "Naughty Dog", "Insomniac", "Santa Monica"},
+            {"Adventure", "Golden Eye", "Spyro", "Pong"},
+            {"NES", "Gameboy", "Nintendo 64", "Gamecube"}
+    };
+
+    private String mCorrectMath [] = {
             "11", "12", "6", "heptagon", "100"
     };
 
-    public String getQuestion(int a) {
-        String question = mQuestions[a];
+    private String mCorrectVG [] = {
+            "1995", "Rainbow Road", "Santa Monica", "Adventure", "Gameboy"
+    };
+
+    public String getQuestionVG(int a) {
+        String question = mQuestionsVG[a];
         return question;
     }
 
-    public String getChoice1(int a) {
-        String choice1 = mChoices[a][0];
+    public String getChoice1VG(int a) {
+        String choice1 = mChoicesVG[a][0];
         return choice1;
     }
 
-    public String getChoice2(int a) {
-        String choice2 = mChoices[a][1];
+    public String getChoice2VG(int a) {
+        String choice2 = mChoicesVG[a][1];
         return choice2;
     }
 
-    public String getChoice3(int a) {
-        String choice3 = mChoices[a][2];
+    public String getChoice3VG(int a) {
+        String choice3 = mChoicesVG[a][2];
         return choice3;
     }
 
-    public String getChoice4(int a) {
-        String choice4 = mChoices[a][3];
+    public String getChoice4VG(int a) {
+        String choice4 = mChoicesVG[a][3];
         return choice4;
     }
 
-    public String getCorrect(int a) {
-        String answer = mCorrect[a];
+    public String getCorrectVG(int a) {
+        String answer = mCorrectVG[a];
+        return answer;
+    }
+
+    public String getQuestionMath(int a) {
+        String question = mQuestionsMath[a];
+        return question;
+    }
+
+    public String getChoice1Math(int a) {
+        String choice1 = mChoicesMath[a][0];
+        return choice1;
+    }
+
+    public String getChoice2Math(int a) {
+        String choice2 = mChoicesMath[a][1];
+        return choice2;
+    }
+
+    public String getChoice3Math(int a) {
+        String choice3 = mChoicesMath[a][2];
+        return choice3;
+    }
+
+    public String getChoice4Math(int a) {
+        String choice4 = mChoicesMath[a][3];
+        return choice4;
+    }
+
+    public String getCorrectMath(int a) {
+        String answer = mCorrectMath[a];
         return answer;
     }
 }

9da468ad113b15638d4c989c6033d0e5b3e364ca
Ben Brown <46798091+BenB049@users.noreply.github.com>
2019-12-06 23:53:55 -0600

    changed UI for trivia game and added some code

--- app/src/main/java/com/example/wakeandgame/PicturePuzzle.java
+++ app/src/main/java/com/example/wakeandgame/PicturePuzzle.java
@@ -2,6 +2,7 @@ package com.example.wakeandgame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,7 +22,7 @@ public class PicturePuzzle extends AppCompatActivity {
 
         //Intent intent = getIntent();
 
-        ArrayList<String> mathCategory = new ArrayList<>();
+        final ArrayList<String> mathCategory = new ArrayList<>();
         ArrayList<String> videoGameCategory = new ArrayList<>();
         ArrayList<String> codingCategory = new ArrayList<>();
         ArrayList<String> moviesCategory = new ArrayList<>();
@@ -42,12 +43,65 @@ public class PicturePuzzle extends AppCompatActivity {
         Button category3 = findViewById(R.id.category3);
         Button category4 = findViewById(R.id.category4);
 
-        Button answer1 = findViewById(R.id.answer1);
-        Button answer2 = findViewById(R.id.answer2);
-        Button answer3 = findViewById(R.id.answer3);
-        Button answer4 = findViewById(R.id.answer4);
+        final Button answer1 = findViewById(R.id.answer1);
+        final Button answer2 = findViewById(R.id.answer2);
+        final Button answer3 = findViewById(R.id.answer3);
+        final Button answer4 = findViewById(R.id.answer4);
 
-        TextView question = findViewById(R.id.question);
+        final TextView question = findViewById(R.id.question);
+        TextView questionTV = findViewById(R.id.questionTV);
+        final TextView scoreTV = findViewById(R.id.scoreTV);
+
+        final boolean[] cat1 = {false};
+        final boolean[] cat2 = {false};
+        final boolean[] cat3 = {false};
+        final boolean[] cat4 = {false};
+
+        category1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cat1[0] = true;
+                question.setText(mathCategory.get(0));
+
+                if(cat1[0] == true) {
+                    answer1.setText("one test");
+                    answer2.setText("two test");
+                    answer3.setText("three test");
+                    answer4.setText("four test");
+                }
+
+                answer1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        scoreTV.setText("SCORE: 1/10");
+                    }
+                });
+            }
+        });
+
+
+
+        category2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cat2[0] = true;
+            }
+        });
+
+        category3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cat3[0] = true;
+            }
+        });
+
+        category4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cat4[0] = true;
+            }
+        });
+
 
     }
 }
